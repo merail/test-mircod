5d2b05d6c98624891e3d94ca71ee9441ce19edbc
Мещеров Раиль Наильевич <meshcherov.rail@itmh.ru>
2021-10-24 13:15:48 +0300

    add data binding

--- app/src/main/java/me/rail/mircodtest/MainActivity.kt
+++ app/src/main/java/me/rail/mircodtest/MainActivity.kt
@@ -2,6 +2,8 @@ package me.rail.mircodtest
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import me.rail.mircodtest.databinding.ActivityMainBinding
 import me.rail.mircodtest.service.Candidate
 import me.rail.mircodtest.service.ServiceBuilder
 import retrofit2.Call
@@ -9,9 +11,12 @@ import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         val service = ServiceBuilder.build()
 
